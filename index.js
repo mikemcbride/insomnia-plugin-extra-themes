47e63f6f6f8bedd5afd55aea7b49c40ab671fd36
Mike McBride <mmcbride1007@icloud.com>
2018-11-16 10:37:41 -0600

    adds oceanic next

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports.themes = [
   require('./themes/cobalt2'),
   require('./themes/dracula'),
-  require('./themes/electron-highlighter')
+  require('./themes/electron-highlighter'),
+  require('./themes/oceanic-next')
 ];
