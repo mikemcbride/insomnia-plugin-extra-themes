7fe1b09fc2500d72cbc97984a76ab1e4d79cf8d6
David Leininger <davidbleininger@gmail.com>
2020-06-05 22:25:02 -0500

    Add Cobalt Next Theme

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports.themes = [
   require('./themes/cobalt2'),
+  require('./themes/cobalt-next'),
   require('./themes/dracula'),
   require('./themes/early-riser'),
   require('./themes/electron-highlighter'),
---
+++ themes/cobalt-next.js
@@ -0,0 +1,60 @@
+module.exports = {
+  name: 'cobalt-next',
+  displayName: 'Cobalt Next',
+  theme: {
+    background: {
+      default: '#0f1c23',
+      success: '#99c794',
+      notice: '#fac863',
+      warning: '#fac863',
+      danger: '#ed6f7d',
+      surprise: '#c5a5c5',
+      info: '#5a9bcf'
+    },
+    foreground: {
+      default: '#d8dee9',
+      success: '#fff',
+      notice: '#fff',
+      warning: '#fff',
+      danger: '#fff',
+      surprise: '#fff',
+      info: '#fff'
+    },
+    highlight: {
+      default: 'rgba(101, 115, 126, 1)',
+      xxs: 'rgba(101, 115, 126, 0.05)',
+      xs: 'rgba(101, 115, 126, 0.1)',
+      sm: 'rgba(101, 115, 126, 0.15)',
+      md: 'rgba(101, 115, 126, 0.2)',
+      lg: 'rgba(101, 115, 126, 0.25)',
+      xl: 'rgba(101, 115, 126, 0.3)'
+    },
+    styles: {
+      sidebar: {
+        background: {
+          default: '#0f1c23'
+        }
+      },
+      dialog: {
+        background: {
+          default: '#1b2b34'
+        }
+      },
+      paneHeader: {
+        background: {
+          success: '#5fb3b3',
+          notice: '#fac863',
+          warning: '#fac863',
+          danger: '#ed6f7d',
+          surprise: '#5fb3b3',
+          info: '#5a9bcf'
+        },
+      },
+      transparentOverlay: {
+        background: {
+          default: 'rgba(27, 43, 52, 0.5)'
+        }
+      }
+    }
+  }
+}
